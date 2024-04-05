1f49ad4c0c9914d3151e8942098caffc6849e8df
adiarhmn <adiauliarahmn@gmail.com>
2024-04-05 09:42:36 +0800

    Update Login

---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { useAuth } from '@/features/auth';
+
+import { LoadingScreen } from '../elements';
+
+export const AdminLayout: React.FC = () => {
+  const { creds } = useAuth();
+
+  if (!creds) return <Navigate to="/login" replace />;
+
+  return (
+    <Suspense fallback={<LoadingScreen />}>
+      <Outlet />
+    </Suspense>
+  );
+};
--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,14 +6,15 @@ import { useAuth } from '@/features/auth';
 import { LoadingScreen } from '../elements';
 
 export const AppLayout: React.FC = () => {
-  const { creds } = useAuth();
+  // const { creds } = useAuth();
 
-  if (!creds) return <Navigate to="/login" replace />;
+  // if (!creds) return <Navigate to="/login" replace />;
 
-  console.log('test');
   return (
     <Suspense fallback={<LoadingScreen />}>
-      <Outlet />
+      <div className="w-full min-h-screen pb-14 mx-auto bg-gradient-to-t from-[#f2f8fd] via-[#f6f9fc] to-[#f6f9fc] max-w-md relative overflow-y-auto overflow-x-hidden">
+        <Outlet />
+      </div>
     </Suspense>
   );
 };
