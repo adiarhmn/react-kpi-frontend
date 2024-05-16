55beb511e723c9c3f13ea02d1486c7c37294f533
dianlucky <dianlucky13@gmail.com>
2024-05-16 10:27:04 +0800

    [Data Pendidikan (Profile)] Fix : memperbaiki error data consume API

--- src/features/employee/api/getEduBackground.ts
+++ src/features/employee/api/getEduBackground.ts
@@ -3,7 +3,12 @@ import axios from 'axios';
 const BaseURL = import.meta.env.VITE_API_URL ?? 'http://192.168.1.110:3000/api';
 
 export async function getEduBackground() {
-  const res = await axios.get(`${BaseURL}/employee_education`);
-  return res.data;
-  console.log(res.data);
+  try {
+    const res = await axios.get(`${BaseURL}/employee_education`);
+    console.log(res.data.data);
+    return res.data.data;
+  } catch (error) {
+    console.error('Error fetching education background:', error);
+    return [];
+  }
 }
