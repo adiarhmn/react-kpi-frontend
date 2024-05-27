f21f661411a7d2f8dcf149ecdc30dfd67d8a44f7
dianlucky <dianlucky13@gmail.com>
2024-05-27 13:06:40 +0800

    [Overtime] Add : Menambahkan fitur lembur

--- src/features/history/pages/DataAttendance.tsx
+++ src/features/history/pages/DataAttendance.tsx
@@ -1,11 +1,25 @@
-import { Table } from '@mantine/core';
+import { ShiftType } from '@/admin_features/types';
+import { useGetShift } from '@/features/schedule/api';
+import { ScheduleList } from '@/features/schedule/components';
+import { Button, Fieldset, Modal, Select, Table } from '@mantine/core';
 import { MonthPickerInput } from '@mantine/dates';
-import { IconChevronLeft } from '@tabler/icons-react';
-import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { IconAdjustmentsHorizontal, IconChevronLeft } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const DataAttendance: React.FC = () => {
-  const [month, setMonth] = useState<Date | null>(new Date());
+  const [opened, { open, close }] = useDisclosure(false);
+  const [month, setMonth] = useState<Date>(new Date());
+  const [selectShift, setSelectShift] = useState('');
+  const [selectStatus, setSelectStatus] = useState('');
+  const [shifts, setShifts] = useState<ShiftType[]>([]);
+  const { data, isLoading, error } = useGetShift();
+  useEffect(() => {
+    if (data) {
+      setShifts(data);
+    }
+  }, [data]);
 
   const navigate = useNavigate();
   return (
@@ -22,89 +36,71 @@ export const DataAttendance: React.FC = () => {
               size={21}
               className="font-bold rounded-md"
             />
-            <h2 className="font-semibold ">Data Absensi</h2>
+            <h2 className="font-semibold ">Riwayat absensi</h2>
           </div>
           <span className="font-semibold">
-            {month?.toLocaleString('default', { month: 'long' })} {month?.getFullYear()}
+            <Button className="shadow-sm" size="xs" onClick={open}>
+              <IconAdjustmentsHorizontal className="me-2 -ms-1" />
+              Filter
+            </Button>
           </span>
         </div>
 
         {/* Month Picker or Input Date */}
         <div>
-          <p className="text-xs text-slate-400 mb-1">Pencarian berdasarkan bulan :</p>
-          <MonthPickerInput size="xs" placeholder="Pick date" value={month} onChange={setMonth} />
+          <div className="w-full grid grid-cols-12">
+            <div className="col-span-12">
+              <p className="text-xs text-slate-400 mb-1">Rekap absensi bulan :</p>
+              <MonthPickerInput
+                size="xs"
+                placeholder="Pick date"
+                value={month}
+                onChange={(value) => {
+                  if (value === null) {
+                    setMonth(new Date());
+                  } else {
+                    setMonth(value);
+                  }
+                }}
+              />
+            </div>
+            <div className="col-span-0"></div>
+          </div>
         </div>
       </section>
 
-      {/* List Attendance Data List */}
-      <section className="px-2 flex flex-col gap-1 text-slate-600 mx-3 mt-4 divide-y divide-slate-300">
-        {/* Header */}
-        <div className="grid grid-cols-4 text-center gap-1 pt-2 text-sm text-blue-700">
-          <div className="text-center font-semibold">Hari</div>
-          <div className="flex gap-1 items-center ps-2 font-semibold">Jam Masuk</div>
-          <div className="flex flex-col ps-2 font-semibold">Jam Keluar</div>
-          <div className="flex flex-col ps-2 font-semibold">Keterangan</div>
-        </div>
+      <ScheduleList month={month} shift={selectShift} status={selectStatus} modalState={opened} />
 
-        {/* Data */}
-        <div className="grid grid-cols-4 text-center gap-1 p-1 text-sm">
-          <div>Jum'at, 13</div>
-          <div className="ps-2">
-            <span>08.13</span>
-            <span className="text-xs bg-red-600 text-white rounded-md m-1 px-1">T</span>
-          </div>
-          <div className="ps-2">
-            <span>08.13</span>
-            <span className="text-xs bg-red-600 text-white rounded-md m-1 px-1">PC</span>
-          </div>
-          <div className="ps-2">
-            <span className="bg-green-300 px-2 rounded-full">Lembur</span>
-          </div>
+      <Modal opened={opened} title="Filter" onClose={close} withCloseButton={false}>
+        <div>
+          {' '}
+          <Fieldset className="mb-2" legend="Shift">
+            <Select
+              className="-m-3"
+              placeholder="Pilih shift"
+              data={['pagi', 'siang', 'malam']}
+              searchValue={selectShift}
+              onSearchChange={setSelectShift}
+              allowDeselect
+            />
+          </Fieldset>
+          <Fieldset className="mb-2" legend="Status">
+            <Select
+              className="-m-3"
+              placeholder="Pilih status"
+              data={['on', 'off']}
+              searchValue={selectStatus}
+              onSearchChange={setSelectStatus}
+              allowDeselect
+            />
+          </Fieldset>
         </div>
-
-        <div className="grid grid-cols-4 text-center gap-1 p-1 text-sm">
-          <div>Sabtu, 14</div>
-          <div className="ps-2">
-            <span>08.13</span>
-            <span className="text-xs bg-red-600 text-white rounded-md m-1 px-1">T</span>
-          </div>
-          <div className="ps-2">
-            <span>08.13</span>
-            <span className="text-xs bg-red-600 text-white rounded-md m-1 px-1">PC</span>
-          </div>
-          <div className="ps-2">
-            <span className="bg-red-300 px-2 rounded-full">Lembur</span>
-          </div>
+        <div className="text-right mt-3">
+          <Button onClick={close} style={{ width: '160px' }}>
+            Cari
+          </Button>
         </div>
-
-        <div className="grid grid-cols-4 text-center gap-1 p-1 text-sm bg-red-200">
-          <div>Minggu, 15</div>
-          <div className="ps-2">-</div>
-          <div className="ps-2">-</div>
-          <div className="ps-2">-</div>
-        </div>
-      </section>
-
-      {/* <section className="px-2">
-        <Table>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>Element position</Table.Th>
-              <Table.Th>Element name</Table.Th>
-              <Table.Th>Symbol</Table.Th>
-              <Table.Th>Atomic mass</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            <Table.Tr>
-              <Table.Th>Element position</Table.Th>
-              <Table.Th>Element name</Table.Th>
-              <Table.Th>Symbol</Table.Th>
-              <Table.Th>Atomic mass</Table.Th>
-            </Table.Tr>
-          </Table.Tbody>
-        </Table>
-      </section> */}
+      </Modal>
     </main>
   );
 };
