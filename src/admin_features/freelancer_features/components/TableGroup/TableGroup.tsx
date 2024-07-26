bf1622cf548852daf86ad8e2dc7de18df0c7adcc
adiarhmn <adiauliarahmn@gmail.com>
2024-07-26 08:26:45 +0800

    Create : Feature See Detail Group

--- src/admin_features/freelancer_features/components/TableGroup/TableGroup.tsx
+++ src/admin_features/freelancer_features/components/TableGroup/TableGroup.tsx
@@ -1,5 +1,7 @@
-import { ActionIcon, Button, Table } from '@mantine/core';
+import { ActionIcon, Button, Modal, Table } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { IconEye, IconTrash } from '@tabler/icons-react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { GroupType } from '@/admin_features/types';
@@ -12,11 +14,20 @@ export const TableGroup: React.FC = () => {
   const navigate = useNavigate();
   if (!creds) navigate('./login');
 
+  const [opened, { open, close }] = useDisclosure();
+  const [detailGroup, setDetailGroup] = useState<GroupType | undefined>(undefined);
+  const DetailGroupChange = (group: GroupType) => {
+    setDetailGroup(group);
+    setTimeout(() => {
+      open();
+    }, 50);
+  };
+
+  console.log('Detail Group', detailGroup);
   const { data, isLoading, isError } = useGetGroup(creds?.company_id || 0);
   if (isLoading) return <div>Loading</div>;
   if (isError) return <div>Error</div>;
 
-  console.log('Data Kelompok', data);
   return (
     <div>
       <Table withColumnBorders withTableBorder>
@@ -48,7 +59,12 @@ export const TableGroup: React.FC = () => {
                     <ActionIcon color="red">
                       <IconTrash size={14} />
                     </ActionIcon>
-                    <Button size="xs" color="blue" leftSection={<IconEye size={14} />}>
+                    <Button
+                      onClick={() => DetailGroupChange(group)}
+                      size="xs"
+                      color="blue"
+                      leftSection={<IconEye size={14} />}
+                    >
                       Detail
                     </Button>
                   </Table.Td>
@@ -58,6 +74,76 @@ export const TableGroup: React.FC = () => {
           )}
         </Table.Tbody>
       </Table>
+
+      {/* Modal Detail Groups */}
+      <Modal
+        size={'lg'}
+        opened={opened}
+        onClose={close}
+        title={<div className="font-semibold">Detail {detailGroup?.name || 'Group'}</div>}
+      >
+        <div>
+          <div className="font-semibold flex gap-2 mb-5">
+            {' '}
+            <div>Nama Kelompok : </div>
+            {detailGroup?.name}
+          </div>
+          <div className="mb-5">
+            <div className="font-semibold text-xs">Anggota Kelompok</div>
+            <table className="w-full text-sm text-center">
+              <thead>
+                <tr>
+                  <th className="border border-slate-300 font-semibold">Nama Pekerja</th>
+                  <th className="border border-slate-300 font-semibold">Kode Pekerja</th>
+                  <th className="border border-slate-300 font-semibold">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {detailGroup?.EmployeeGroups.map((employee, index) => (
+                  <tr key={index}>
+                    <td className="border border-slate-300 p-1">{employee.employee.name}</td>
+                    <td className="border border-slate-300 p-1">{employee.employee.nip || '-'}</td>
+                    <td className="border border-slate-300 p-1 text-center">
+                      <ActionIcon color="red">
+                        <IconTrash size={14} />
+                      </ActionIcon>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <div className="font-semibold text-xs">Sesi Kerja</div>
+            <table className="w-full text-sm text-center">
+              <thead>
+                <tr>
+                  <th className="border border-slate-300 font-semibold">Nama Sesi</th>
+                  <th className="border border-slate-300 font-semibold">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {detailGroup?.GroupSessions.map((session, index) => (
+                  <tr key={index}>
+                    <td className="border border-slate-300 p-1">{session.session.name}</td>
+                    <td className="border border-slate-300 p-1 text-center">
+                      <ActionIcon color="red">
+                        <IconTrash size={14} />
+                      </ActionIcon>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className="mt-10 flex justify-end">
+          <Button color="gray" onClick={close}>
+            Tutup Detail
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
